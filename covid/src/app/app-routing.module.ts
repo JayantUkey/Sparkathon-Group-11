fca5ba72d08235f3f0f4eed71b2f3a383799cc2b
mansh-cell <mishramanshi14@gmail.com>
2022-09-24 09:59:30 +0530

    changes

--- covid/src/app/app-routing.module.ts
+++ covid/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { components } from 'knockout';
+import { AppointmentComponent } from './user/appointment/appointment.component';
+import { HomeComponent } from './user/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component:HomeComponent },
+  {path:"/appointment",component:AppointmentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- covid/src/app/app.module.ts
+++ covid/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './user/home/home.component';
+import { HeaderComponent } from './user/header/header.component';
+import { AppointmentComponent } from './user/appointment/appointment.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    AppointmentComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
